abeb8b0785205264714d867847146cb8bcb3c62c
Love Khatri <lovekhatri9211@gmail.com>
2024-07-05 12:45:44 +0530

    UPDATE: user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import protectRoute from '../middlewares/protectRoute';
+import { getUsersForSidebar } from '../controllers/user.controller.js';
 
 const userRouter = express.Router();
 
